1c974968217a87bcda79b0df70a4e262871fd801
fjcorrales <92602420+fjcorrales@users.noreply.github.com>
2022-05-13 10:32:27 +0200

    supuesta corrección de la fase 3
    implementado un for que genera un chunk por cada réplica del fichero

--- miniGFS.2022/master_node/src/master/FileImpl.java
+++ miniGFS.2022/master_node/src/master/FileImpl.java
@@ -32,14 +32,15 @@ public class FileImpl extends UnicastRemoteObject implements File {
 	// obtiene información de ubicación de los chunks especificados del fichero
 	public List <Chunk> getChunkDescriptors(int nchunk, int size) throws RemoteException {
 		List<Chunk> infoChunks = new ArrayList<Chunk>();					//Lista con los chunks de informacion que se va a devolver
-
+		int aux = nchunk;
 //	For para ir escanando el mapa para ver si se encuentran chunks ya creados en ese rango
-		for(int i = nchunk ; i<size ; i++){
-			if(mapaChunks.containsKey(i)){							//Si hay chunks en ese intervalo, los añado a la lista que voy a devolcer
-				infoChunks.add(mapaChunks.get(i));
+		for(int i = 0 ; i<size ; i++){
+			if(mapaChunks.containsKey(aux)){							//Si hay chunks en ese intervalo, los añado a la lista que voy a devolcer
+				infoChunks.add(mapaChunks.get(aux));
 			}else{										//si no esta en el mapa, se pone un null
 				infoChunks.add(null);
 			}
+			aux++;
 		}
 		return infoChunks;
 	}
@@ -48,16 +49,21 @@ public class FileImpl extends UnicastRemoteObject implements File {
 	public List <Chunk> allocateChunkDescriptors(int nchunk, int size) throws RemoteException {
 		ArrayList<DataNode> nuevoChunk = new ArrayList<DataNode>();
 		ArrayList<Chunk> chunkList = new ArrayList<Chunk>();
-		for(int i = nchunk ; i<size ; i++){
-			if(!mapaChunks.containsKey(i)){
+		int aux = nchunk;
+		for(int i = 0 ; i<size ; i++){
+			if(!mapaChunks.containsKey(aux)){
 				/*
 					Si no existe ese chunk, no tiene info, no está en el mapa, se crea y se añade
 				*/
 				nuevoChunk.add(manager.selectDataNode());
 			}
+			aux++;
 		}
-		ChunkImpl nuevo = new ChunkImpl(nuevoChunk);
-		mapaChunks.put(nchunk, nuevo);
+		for(int i = 0 ; i<repFact ; i++){
+			//Por cada rplica que tengamos del fichero creamos un chunk distinto
+			ChunkImpl nuevo = new ChunkImpl(nuevoChunk);
+			mapaChunks.put(nchunk, nuevo);
+		}
 		for(Integer key:mapaChunks.keySet()){
 			chunkList.add(mapaChunks.get(key));
 		}
