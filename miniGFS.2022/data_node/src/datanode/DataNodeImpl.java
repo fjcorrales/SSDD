455c5a2c7836e1bd085bd9d76573fd2a3c1050db
fjcorrales <92602420+fjcorrales@users.noreply.github.com>
2022-05-13 10:34:25 +0200

    Fase 1
    Implementada por Santi, no se que hace XD

--- miniGFS.2022/data_node/src/datanode/DataNodeImpl.java
+++ miniGFS.2022/data_node/src/datanode/DataNodeImpl.java
@@ -4,17 +4,24 @@ package datanode;
 import java.util.List;
 import java.rmi.*;
 import java.rmi.server.*;
+import java.io.*;
 
 import interfaces.*;
 
 public class DataNodeImpl extends UnicastRemoteObject implements DataNode {
     public static final long  serialVersionUID=1234567891;
 
+	private String name;
+	private Manager man;
+
     public DataNodeImpl(Manager m, String n) throws RemoteException {
+    	name = n;
+    	man = m;
+    	man.addDataNode(this);
     }
     // nombre del nodo
     public String getName() throws RemoteException {
-        return null;
+        return name;
     }
     // lee el fichero que contiene un chunk
     public byte [] readChunk(String chunkName) throws RemoteException {
@@ -24,6 +31,16 @@ public class DataNodeImpl extends UnicastRemoteObject implements DataNode {
     // si la lista de nodos pasada como parámetro no esta vacía,
     // propaga la escritura a los nodos de datos de la lista
     public boolean writeChunk(List <DataNode> nodes, String chunkName, byte [] buffer) throws RemoteException {
-        return true;
+    	boolean resultado = false;
+    	if(nodes == null){
+    		try {
+			FileOutputStream fichero =  new FileOutputStream(chunkName);
+			fichero.write(buffer);
+			resultado = true;
+		} catch (Exception e){
+			System.out.println("Error en la escritura de un chunk");
+		}
+    	}
+        return resultado;
     }
 }
