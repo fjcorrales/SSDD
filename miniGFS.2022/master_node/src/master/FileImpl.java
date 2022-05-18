d5efef00f9c6adc7644081b25895027903632a2f
Daniel_Corrales <92602420+fjcorrales@users.noreply.github.com>
2022-05-19 00:21:09 +0200

    Inicio de fase 4
    Primer intento de implementacion de  la fase 4, funcion "getChunkDescriptors", puede que como en "allocateChunkDescriptors" haya que añadir un for para cargar los chunks que no estén en el rango pedido en la llamada a la funcion

--- miniGFS.2022/master_node/src/master/FileImpl.java
+++ miniGFS.2022/master_node/src/master/FileImpl.java
@@ -31,32 +31,33 @@ public class FileImpl extends UnicastRemoteObject implements File {
 
 	// obtiene información de ubicación de los chunks especificados del fichero
 	public List <Chunk> getChunkDescriptors(int nchunk, int size) throws RemoteException {
-		List<Chunk> infoChunks = new ArrayList<Chunk>();					//Lista con los chunks de informacion que se va a devolver
-
-//	For para ir escanando el mapa para ver si se encuentran chunks ya creados en ese rango
-		for(int i = nchunk ; i<size ; i++){
-			if(mapaChunks.containsKey(i)){							//Si hay chunks en ese intervalo, los añado a la lista que voy a devolcer
-				infoChunks.add(mapaChunks.get(i));
-			}else{										//si no esta en el mapa, se pone un null
-				infoChunks.add(null);
+		List<Chunk> result = new ArrayList<Chunk>();
+		for(int i = nchunk ; i<nchunk + size ; i++){
+			if(!mapaChunks.containsKey(i)){							/*Si no existen chunks, pero estamos dentro del rango del fichero,
+													añadimos un null en la posicion de la lista que se retorna*/
+				mapaChunks.put(i, null);//puede que no sea necesario
+				result.add(null);
+			}else{										//Si existe el chunk, lo añado a la lista resultado
+				result.add(mapaChunks.get(i));
 			}
 		}
-		return infoChunks;
+		return result;
 	}
 
 	// reserva información de ubicación para los chunks especificados del fichero
 	public List <Chunk> allocateChunkDescriptors(int nchunk, int size) throws RemoteException {
 		List<Chunk> result = new ArrayList<Chunk>();
 
+		//For para cargar todo el fichero en el mapa de chunks, los chunks de fuera del rango pedido
 		for(int i = nchunk ; i<nchunk ; i++){
 			if(!mapaChunks.containsKey(i)){
 				List<DataNode> dataNode = new ArrayList<DataNode>();
-				for(int j = 0 ; j<repFact ; j++){
+				for(int j = 0 ; j<repFact ; i++){
 					DataNode nuevo = manager.selectDataNode();
 					dataNode.add(nuevo);
 				}
 				ChunkImpl newChunk = new ChunkImpl(dataNode);
-				mapaChunks.add(newChunk);
+				mapaChunks.put(i, newChunk);
 			}
 		}
 
@@ -64,12 +65,12 @@ public class FileImpl extends UnicastRemoteObject implements File {
 			if(!mapaChunks.containsKey(i)){							//Si no existen chunks, creo el chunk y lo añado al mapa
 				List<DataNode> dataNode = new ArrayList<DataNode>();
 				//for para hacer nueva lista de nodos por replica
-				for(int j = 0 ; j<repFact ; j++){
+				for(int j = 0 ; j<repFact ; i++){
 					DataNode nuevo = manager.selectDataNode();
 					dataNode.add(nuevo);
 				}
 				ChunkImpl newChunk = new ChunkImpl(dataNode);
-				mapaChunks.add(newChunk);
+				mapaChunks.put(i, newChunk);
 				result.add(newChunk);
 			}else{										//Si existe el chunk, lo añado a la lista resultado
 				result.add(mapaChunks.get(i));
@@ -78,4 +79,3 @@ public class FileImpl extends UnicastRemoteObject implements File {
 		return result;
 	}
 }
-
