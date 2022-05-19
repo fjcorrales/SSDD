9074eb58206ff69323d0af08c336a0cc03883476
Daniel_Corrales <92602420+fjcorrales@users.noreply.github.com>
2022-05-19 12:17:15 +0200

    fase 4
    modificación al metodo getChunkDescriptors

--- miniGFS.2022/master_node/src/master/FileImpl.java
+++ miniGFS.2022/master_node/src/master/FileImpl.java
@@ -32,10 +32,15 @@ public class FileImpl extends UnicastRemoteObject implements File {
 	// obtiene información de ubicación de los chunks especificados del fichero
 	public List <Chunk> getChunkDescriptors(int nchunk, int size) throws RemoteException {
 		List<Chunk> result = new ArrayList<Chunk>();
-		for(int i = nchunk ; i<nchunk + size ; i++){
-			if(!mapaChunks.containsKey(i)){							/*Si no existen chunks, pero estamos dentro del rango del fichero,
-													añadimos un null en la posicion de la lista que se retorna*/
-				mapaChunks.put(i, null);//puede que no sea necesario
+		if(!mapaChunks.containsKey(i)){							//Si no existen chunks, creo el chunk y lo añado al mapa
+				List<DataNode> dataNode = new ArrayList<DataNode>();
+				//for para hacer nueva lista de nodos por replica
+				for(int j = 0 ; j<repFact ; j++){
+					DataNode nuevo = manager.selectDataNode();
+					dataNode.add(nuevo);
+				}
+				ChunkImpl newChunk = new ChunkImpl(dataNode);
+				mapaChunks.put(i, newChunk);
 				result.add(null);
 			}else{										//Si existe el chunk, lo añado a la lista resultado
 				result.add(mapaChunks.get(i));
@@ -48,7 +53,7 @@ public class FileImpl extends UnicastRemoteObject implements File {
 	public List <Chunk> allocateChunkDescriptors(int nchunk, int size) throws RemoteException {
 		List<Chunk> result = new ArrayList<Chunk>();
 
-		//For para cargar todo el fichero en el mapa de chunks, los chunks de fuera del rango pedido
+		//For para cargar "todo" el fichero en el mapa de chunks para evitar el fallo de "tamaño"
 		for(int i = nchunk ; i<nchunk ; i++){
 			if(!mapaChunks.containsKey(i)){
 				List<DataNode> dataNode = new ArrayList<DataNode>();
@@ -79,3 +84,4 @@ public class FileImpl extends UnicastRemoteObject implements File {
 		return result;
 	}
 }
+
