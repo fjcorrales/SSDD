24f1e6ef5c89f209b8dcb41d90963d3e1ece0ad9
Daniel_Corrales <92602420+fjcorrales@users.noreply.github.com>
2022-05-31 21:32:56 +0200

    FIN

--- miniGFS.2022/client_node/src/client/GFSFile.java
+++ miniGFS.2022/client_node/src/client/GFSFile.java
@@ -1,9 +1,11 @@
+
 // Clase de cliente que proporciona los métodos para acceder a los ficheros.
 // Corresponde al API ofrecida a las aplicaciones 
 
 package client;
 import java.rmi.*;
 import interfaces.*;
+import java.util.*;
 
 public class GFSFile {
 
@@ -11,24 +13,27 @@ public class GFSFile {
     private String port,host;
     private Master m;
     private int pointer;
+    private int chunkSize;
 
 
     public GFSFile(String fileName){
     	pointer = 0;
-    	String p = "GFSPort";
-    	String h = "GFSHost";
-    	port = java.lang.System.getenv(p);
-	host = java.lang.System.getenv(h);
+    	String p = "BROKER_PORT";
+    	String h = "BROKER_HOST";
+    	port = System.getenv(p);
+	host = System.getenv(h);
+	chunkSize = Integer.parseInt(java.lang.System.getenv("CHUNKSIZE"));
 	try{
-		m = (Master) Naming.lookup("//" + h + ":" + p + "/GFS_master");
+		m = (Master) Naming.lookup("//" + host + ":" + port + "/GFS_master");
     		file = m.lookup(fileName);
     	} catch (Exception e){
-		System.out.println("Error en el constructor de GFSFile");
+		System.out.println("Error en el constructor GFSFile");
+		e.printStackTrace();
     	}
     }
     // establece la posición de acceso al fichero
     public void seek(int off) {
-    	pointer = off;
+    	 pointer = off;
     }
     // obtiene la posición de acceso al fichero
     public int getFilePointer() {
@@ -38,7 +43,7 @@ public class GFSFile {
     public int length() throws RemoteException {
 	int resultado=0;
 	try{
-		resultado = file.getNumberOfChunks() * 64000000;
+		resultado = file.getNumberOfChunks() * chunkSize;
 	} catch (Exception e){
 		System.out.println("Error en length");
 	}
@@ -48,12 +53,51 @@ public class GFSFile {
     // devuelve cantidad realmente leída, 0 si EOF;
     // actualiza la posición de acceso al fichero
     public int read(byte [] buf) throws RemoteException {
-        return 0;
+       int leido = 0;
+       int nChunks=buf.length/chunkSize;
+       ArrayList<Chunk> listaChunks;
+       ArrayList<DataNode> listaDataNodes;
+       Chunk sigChunk;
+       byte[] bLeidos;
+       int ultEsc = 0;
+       try{
+		listaChunks = (ArrayList<Chunk>) file.getChunkDescriptors(pointer/chunkSize,nChunks);		
+		for(int i=0;i<listaChunks.size();i++){
+			sigChunk = listaChunks.get(i);
+			if(sigChunk != null){
+				bLeidos = sigChunk.getChunkDataNodes().get(0).readChunk(sigChunk.getChunkName());
+				for(int j = 0;j<chunkSize;j++){
+					buf[j+ultEsc] = bLeidos[j];
+				}
+				ultEsc = ultEsc + chunkSize;
+			} else {
+				for(int j=ultEsc;j<ultEsc+chunkSize;j++){
+					buf[j]=0;
+				}
+				ultEsc = ultEsc + chunkSize;
+			}
+			leido++;
+		}
+       } catch(Exception e){
+		System.out.println("Error en el read del cliente");
+		leido = 0;
+       }
+       pointer = pointer + leido*chunkSize;
+        return leido*chunkSize;
     }
     // escribe en la posición actual del fichero los datos especificados;
     // devuelve falso si se ha producido un error en writeChunk;
     // actualiza la posición de acceso al fichero
     public boolean write(byte [] buf) throws RemoteException {
+       int nChunks=buf.length/chunkSize;
+       List<Chunk> listaChunks = file.allocateChunkDescriptors(pointer/chunkSize,nChunks);
+       for(int i=0;i<listaChunks.size();i++){
+		List<DataNode> listaDataNodes = listaChunks.get(i).getChunkDataNodes();
+		DataNode primero = listaDataNodes.remove(0);
+		byte[] buffer = Arrays.copyOfRange(buf,i*chunkSize,(i+1)*chunkSize);
+		primero.writeChunk(listaDataNodes,listaChunks.get(i).getChunkName(),buffer);
+	}
+	pointer = pointer+buf.length;
         return true;
     }
 }
