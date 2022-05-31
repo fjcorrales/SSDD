7c7fd7e6abae36ce3d0c1f778ea3e19468bcbf92
Daniel_Corrales <92602420+fjcorrales@users.noreply.github.com>
2022-05-31 21:31:52 +0200

    FIN

--- miniGFS.2022/master_node/src/master/MasterImpl.java
+++ miniGFS.2022/master_node/src/master/MasterImpl.java
@@ -5,14 +5,26 @@ import java.rmi.*;
 import java.rmi.server.*;
 import manager.*;
 import interfaces.*;
+import java.util.*;
 
 public class MasterImpl extends UnicastRemoteObject implements Master {
     public static final long  serialVersionUID=1234567891;
 
+	private Map<String,FileImpl> mapa;
+	private ManagerImpl manager;
+	private int rep;
+	
     public MasterImpl(ManagerImpl m, int replica) throws RemoteException {
+    	mapa = new HashMap<String, FileImpl>();
+    	manager = m;
+    	rep = replica;
     }
     // obtiene acceso a la metainformación de un fichero
     public synchronized File lookup(String fname) throws RemoteException {
-        return null;
+        if(!mapa.containsKey(fname)){
+        	FileImpl nuevo = new FileImpl(manager, rep);
+        	mapa.put(fname,nuevo);
+        }
+        return mapa.get(fname);
     }
 }
