3db884f5e37a556e747640194290d6ae2c7aa59e
Daniel_Corrales <92602420+fjcorrales@users.noreply.github.com>
2022-05-31 21:32:12 +0200

    FIN

--- miniGFS.2022/master_node/src/manager/ManagerImpl.java
+++ miniGFS.2022/master_node/src/manager/ManagerImpl.java
@@ -11,18 +11,32 @@ import interfaces.*;
 public class ManagerImpl extends UnicastRemoteObject implements Manager {
     public static final long  serialVersionUID=1234567891;
 
+	private ArrayList<DataNode> allDataNodes;
+	private int lastDataNode;
+
     public ManagerImpl() throws RemoteException {
+    	allDataNodes = new ArrayList<>();
+    	lastDataNode = 0;
     }
     // alta de un nodo de datos
     public synchronized void addDataNode(DataNode n) throws RemoteException {
+    	allDataNodes.add(n);
     }
     // obtiene lista de nodos de datos del sistema
     public synchronized List <DataNode> getDataNodes() throws RemoteException {
-        return null;
+        return allDataNodes;
     }
     // método no remoto que selecciona un nodo de datos para ser usado
     // para almacenar un chunk
     public synchronized DataNode selectDataNode() {
-        return null;
+    	DataNode result;
+    	if(lastDataNode < allDataNodes.size()){
+    		result = allDataNodes.get(lastDataNode);
+    	} else {
+    		lastDataNode = 0;
+    		result = allDataNodes.get(lastDataNode);
+    	}
+    	lastDataNode++;
+        return result;
     }
 }
