1b2252acf064ff0cb66a1033a0ee581b20363326
Daniel_Corrales <92602420+fjcorrales@users.noreply.github.com>
2022-06-29 11:23:12 +0200

    2,5 completado
    Implementación correcta de la conexión entre broker y cliente, así como función clients(), además de la lectura del fichero de temas y creación del listado de los mismos, así como función topics()

--- edsupull.2022/broker/broker.c
+++ edsupull.2022/broker/broker.c
@@ -16,15 +16,15 @@
 
 //Cuando se crea el thread, llama a esta funcion de rutina "el main"
 
-//////////////////VATIABLES//////////////////////
+//////////////////VARIABLES//////////////////////
 
-//	set* temasList;			//set en el cual guardo los temas del fichero temas
-//	temasList = set_create(0);
+set *temasList;			//set en el cual guardo los temas del fichero temas
 
 //	map* subscripciones;		//mapa en el que asocio a un cliente con un conjunto de sus subscripciones
 //	subscripciones = map_create(key_string, 0); //la key sera el uuid del cliente en cuestion y su valor asociado el set
 
-	map *clientes;			//mapa para guardar los clientes
+map *clientes;			//mapa para guardar los clientes
+FILE *f;
 ////////////////////////////////////////////////
 
 struct cliente{
@@ -36,7 +36,7 @@ struct cliente{
 void *servicio(void *arg){
 
 	int sockfd, leido;		//nclient sera el numero de cliente
-	char tam[100], buff[100];
+	char tam[100], buff[sizeof(int)], tema[250];
         sockfd=(long) arg;
 	if(leido = recv(sockfd, tam, 100, 0)){
 		//Si he conseguido leer un uuid, lo añado al mapa en su posicion
@@ -47,24 +47,45 @@ void *servicio(void *arg){
 		map_put(clientes, nuevo->id, nuevo);		//en el mapa de clientes guardo el id del cliente como key y valor un objeto de tipo cliente
 	}
 	//enviar un 1 es respuesta ok
-	printf("UID cliente: %s", tam);
+	printf("UID cliente: %s\n", tam);
 	char* resp = "1";
         if(write(sockfd, resp, strlen(resp))<0){
 		perror("[ERROR THREAD BROKER] no ha podido mandar respuesta\n");
 	}
+
+	printf("BROKER EMPIEZA A LEER EL FICHERO TEMAS\n");
+	//antes de entrar al bucle de servicio, leo los temas del fichero temas
+	while((fgets(tema, 250, f))!=NULL){//while para ir sacando linea por linea los temas
+		char *topic = strdup(tema);
+		if(set_add(temasList, topic)!=0){
+			printf("TEMA NO AÑADIDO\n");
+		}else{
+			printf("TEMA AÑADIDO-> %s", tema);
+		}
+	}
+	close(f);
+	printf("BORKER TERMINO DE LEER Y AÑADIR LOS TEMAS, CIERRA EL FICHERO\n");
+
 	while(1){//espera activa para peticiones
 		//para tema peticiones y clients...
-		recv(sockfd, buff, 100, 0);//guardar peticion en el buffer
+		recv(sockfd, buff, sizeof(int), 0);//guardar peticion en el buffer
 		//switch para ditinguir que peticiones
 		int pet = atoi(buff);
-		memset(buff, 0, 100);//reinicio el buffer
+		memset(buff, 0, sizeof(int));//reinicio el buffer
 		switch(pet){
 			case 1:	{		//caso en que me pidan el nº de clientes
 				int nclientes = map_size(clientes);
 				sprintf(buff, "%d", nclientes);
-				send(sockfd, buff, 100, 0);
+				send(sockfd, buff, sizeof(int), 0);
+				memset(buff, 0, sizeof(int));//reinicio el buffer
 			}
 
+			case 2: {
+				int nTemas = set_size(temasList);
+				sprintf(buff, "%d", nTemas);
+				send(sockfd, buff, sizeof(int), 0);
+				memset(buff, 0, sizeof(int));//reinicio el buffer
+			}
 		}
 	}
 	close(sockfd);
@@ -85,16 +106,22 @@ int main(int argc, char *argv[]){
 	}
 
 	clientes = map_create(key_string, 0);
+	temasList = set_create(0);
 
 	//creamos el socket
 	int sockfd, connfd;				//descriptores tanto de la conexion como de la conexion
 	unsigned int len;				//es la longitud de la direccion del cliente
-	//int rx_long, tx_long = 0;			//la longitud en bytes de lo que llega y de lo que se envia
-	//int nclients = clients();			//para saber el nº de clientes que hay en este momento en el sistema SEGURAMENTE NO FUNCIONE
 	struct sockaddr_in servaddr, clientaddr;
 	char* host = getenv("BROKER_HOST");
 	char* puerto = argv[1];
 	int op = 1;
+	printf("BROKER ABRE EL FICHERO TEMAS.TXT\n");
+	//abro el fichero de temas, si no puedo envio mensaje de error
+	f = fopen(argv[2], "r");
+	if(f==NULL){
+		perror("[ERROR SERVER] no se ha podido abrir el fichero de temas");
+	}
+	printf("BROKER ACABA DE ABRIR EL FICHERO TEMAS.TXT\n");
 
 	//creo socket
 	sockfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -137,20 +164,6 @@ int main(int argc, char *argv[]){
     	pthread_attr_init(&atrib_th); // evita pthread_join
     	pthread_attr_setdetachstate(&atrib_th, PTHREAD_CREATE_DETACHED);
 
-	//leemos el fichero de temas y nos llenamos un conjunto con todos los temas
-//	FILE *f;
-//	f = fopen("temas", "r");
-//	char temasAux[2048];
-//	while(fgets(temasAux, 2048, f) != NULL){
-//		set_add(temas, temasAux);
-//	}
-
-	//guardamos la cantidad de temas que tenemos y la enviamos al cliente
-//	int ntemas = set_size(temas);
-//	if(write(sockfd, &ntemas, sizeof(ntemas))<0){
-//		perror("[ERROR BROKER] error al enviar el numero de temas que tenemos");
-//		return -1;
-//	}
 
 	while(1){
 		//Realizo el accept
