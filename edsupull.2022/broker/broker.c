4a7196e7341eda519f3715bc44cb13ec8e0db89b
Daniel_Corrales <92602420+fjcorrales@users.noreply.github.com>
2022-07-02 18:40:44 +0200

    2,5 conseguido

--- edsupull.2022/broker/broker.c
+++ edsupull.2022/broker/broker.c
@@ -18,13 +18,14 @@
 
 //////////////////VARIABLES//////////////////////
 
-set *temasList;			//set en el cual guardo los temas del fichero temas
+map *temasList;			//set en el cual guardo los temas del fichero temas
 
 //	map* subscripciones;		//mapa en el que asocio a un cliente con un conjunto de sus subscripciones
 //	subscripciones = map_create(key_string, 0); //la key sera el uuid del cliente en cuestion y su valor asociado el set
 
 map *clientes;			//mapa para guardar los clientes
-FILE *f;
+struct cabecera head;		//cabecera que tenga actualmente
+
 ////////////////////////////////////////////////
 
 struct cliente{
@@ -33,10 +34,20 @@ struct cliente{
 	queue* eventos;
 };
 
+struct tema{
+	char *nombre;
+	set *subs;
+};
+struct cabecera{
+	int h1;
+	int h2;
+	int h3;//tamanyo de petticion
+};
+//////////////////////////////////////////////
 void *servicio(void *arg){
 
 	int sockfd, leido;		//nclient sera el numero de cliente
-	char tam[100], buff[sizeof(int)], tema[250];
+	char tam[100], buff[sizeof(int)];
         sockfd=(long) arg;
 	if(leido = recv(sockfd, tam, 100, 0)){
 		//Si he conseguido leer un uuid, lo añado al mapa en su posicion
@@ -53,38 +64,84 @@ void *servicio(void *arg){
 		perror("[ERROR THREAD BROKER] no ha podido mandar respuesta\n");
 	}
 
-	printf("BROKER EMPIEZA A LEER EL FICHERO TEMAS\n");
-	//antes de entrar al bucle de servicio, leo los temas del fichero temas
-	while((fgets(tema, 250, f))!=NULL){//while para ir sacando linea por linea los temas
-		char *topic = strdup(tema);
-		if(set_add(temasList, topic)!=0){
-			printf("TEMA NO AÑADIDO\n");
-		}else{
-			printf("TEMA AÑADIDO-> %s", tema);
-		}
-	}
-	close(f);
-	printf("BORKER TERMINO DE LEER Y AÑADIR LOS TEMAS, CIERRA EL FICHERO\n");
-
 	while(1){//espera activa para peticiones
 		//para tema peticiones y clients...
-		recv(sockfd, buff, sizeof(int), 0);//guardar peticion en el buffer
+		recv(sockfd, &head, sizeof(head), 0);//recibo la cabecera
+		int h3=ntohl(head.h3);//normalmente el tamanyo de la preticion
+		char *pet = malloc(h3);
+		recv(sockfd, pet, h3, 0);//guardar peticion
 		//switch para ditinguir que peticiones
-		int pet = atoi(buff);
-		memset(buff, 0, sizeof(int));//reinicio el buffer
-		switch(pet){
-			case 1:	{		//caso en que me pidan el nº de clientes
+		int pet1 = atoi(pet);
+		switch(pet1){
+			case 1:	{//nº de clientes
 				int nclientes = map_size(clientes);
 				sprintf(buff, "%d", nclientes);
 				send(sockfd, buff, sizeof(int), 0);
 				memset(buff, 0, sizeof(int));//reinicio el buffer
+				break;
 			}
 
-			case 2: {
-				int nTemas = set_size(temasList);
+			case 2: {//nº de temas
+				int nTemas = map_size(temasList);
 				sprintf(buff, "%d", nTemas);
 				send(sockfd, buff, sizeof(int), 0);
 				memset(buff, 0, sizeof(int));//reinicio el buffer
+				break;
+			}
+
+			case 3: {//subscripcion a un tema
+				int error;
+				int h1=ntohl(head.h1);//normalmente el tamayo del tema
+				int h2=ntohl(head.h2);//normalmente el tamanyo del usuario (uuid)
+				char *tema2 = malloc(h1+1);			//+1 para el caracter de fin de linea
+				char *usuario = malloc(h2+1);
+				recv(sockfd, tema2, h1, MSG_WAITALL);
+				recv(sockfd, usuario, h2, MSG_WAITALL);
+				tema2[h1]='\0';					//anyado los fin de linea
+				usuario[h2]='\0';
+				printf("El tema es: %s\n", tema2);
+				printf("El cliente es: %s\n", usuario);
+				struct cliente *cl = map_get(clientes, usuario, &error);//encuentro el cliente
+				if(error==-1){
+					printf("[ERROR BROKER] el cliente a subscribir no se ha encontrado\n");
+					continue;
+				}
+				struct tema *tm = map_get(temasList, tema2, &error);//encuentro el tema
+				if(error==-1){
+					printf("[ERROR BROKER] el tema a subscribir no se ha encontrado\n");
+					continue;
+				}
+				if((set_add(cl->subscrito, tm)==-1) || (set_add(tm->subs, cl)==-1)){//anyado el tema a las subscripciones del cliente
+					printf("[ERROR BROKER] no se ha podido suscribir el cliente\n");
+					sprintf(buff, "f");
+					send(sockfd, buff, sizeof(int), 0);
+					memset(buff, 0, sizeof(int));//reinicio el buffer
+					continue;
+				}else{
+					sprintf(buff, "a");
+					send(sockfd, buff, sizeof(int), 0);
+					memset(buff, 0, sizeof(int));//reinicio el buffer
+				}
+				break;
+			}
+
+			case 4: {//nº de subs de un tema
+				int error;
+				int h1=ntohl(head.h1);//normalmente el tamayo del tema
+				char *tema2 = malloc(h1+1);			//+1 para el caracter de fin de linea
+				recv(sockfd, tema2, h1, MSG_WAITALL);
+				tema2[h1]='\0';					//anyado los fin de linea
+				printf("El tema es: %s\n", tema2);
+				struct tema *tm = map_get(temasList, tema2, &error);//encuentro el tema
+				if(error==-1){
+					printf("[ERROR BROKER] el tema a subscribir no se ha encontrado\n");
+					continue;
+				}
+				int nSubs = set_size(tm->subs);
+				sprintf(buff, "%d", nSubs);
+				send(sockfd, buff, sizeof(int), 0);
+				memset(buff, 0, sizeof(int));//reinicio el buffer
+				break;
 			}
 		}
 	}
@@ -93,10 +150,7 @@ void *servicio(void *arg){
 }
 
 //para mas adelante
-struct cabecera {
-	int long1;
-	int long2;
-};
+
 
 int main(int argc, char *argv[]){
 
@@ -106,9 +160,10 @@ int main(int argc, char *argv[]){
 	}
 
 	clientes = map_create(key_string, 0);
-	temasList = set_create(0);
+	temasList = map_create(key_string, 0);
 
 	//creamos el socket
+	char temaB[250];
 	int sockfd, connfd;				//descriptores tanto de la conexion como de la conexion
 	unsigned int len;				//es la longitud de la direccion del cliente
 	struct sockaddr_in servaddr, clientaddr;
@@ -117,6 +172,7 @@ int main(int argc, char *argv[]){
 	int op = 1;
 	printf("BROKER ABRE EL FICHERO TEMAS.TXT\n");
 	//abro el fichero de temas, si no puedo envio mensaje de error
+	FILE *f;
 	f = fopen(argv[2], "r");
 	if(f==NULL){
 		perror("[ERROR SERVER] no se ha podido abrir el fichero de temas");
@@ -164,6 +220,23 @@ int main(int argc, char *argv[]){
     	pthread_attr_init(&atrib_th); // evita pthread_join
     	pthread_attr_setdetachstate(&atrib_th, PTHREAD_CREATE_DETACHED);
 
+
+
+	printf("BROKER EMPIEZA A LEER EL FICHERO TEMAS\n");
+	//antes de entrar al bucle de servicio, leo los temas del fichero temas
+	while((fgets(temaB, 250, f))!=NULL){//while para ir sacando linea por linea los temas
+		char *aux = strdup(temaB);
+		struct tema *topic = malloc(sizeof(struct tema));
+		topic->nombre = aux;
+		topic->subs = set_create(0);
+		if(map_put(temasList, topic->nombre, topic)!=0){
+			printf("TEMA NO AÑADIDO\n");
+		}else{
+			printf("TEMA AÑADIDO-> %s", topic->nombre);
+		}
+	}
+	fclose(f);
+	printf("BORKER TERMINO DE LEER Y AÑADIR LOS TEMAS, CIERRA EL FICHERO\n");
 
 	while(1){
 		//Realizo el accept
